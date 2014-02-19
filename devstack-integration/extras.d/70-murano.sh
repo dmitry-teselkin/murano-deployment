358c24f257c4b822607800c27762a3364f78822d
Dmitry Teselkin <dteselkin@mirantis.com>
2014-02-19 19:01:02 +0400

    Script to install MuranoDsl using devstack

--- devstack-integration/extras.d/70-murano.sh
+++ devstack-integration/extras.d/70-murano.sh
@@ -11,6 +11,9 @@ if is_service_enabled murano; then
         if is_service_enabled horizon; then
             install_murano_dashboard
         fi
+        if is_service_enabled murano-dsl; then
+            install_murano_dsl
+        fi
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         echo_summary "Configuring Murano"
         configure_murano
@@ -18,19 +21,32 @@ if is_service_enabled murano; then
         if is_service_enabled horizon; then
             configure_murano_dashboard
         fi
+        if is_service_enabled murano-dsl; then
+            configure_murano_dsl
+        fi
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Initializing Murano"
         init_murano
         if is_service_enabled horizon; then
             init_murano_dashboard
         fi
+        if is_service_enabled murano-dsl; then
+            init_murano_dsl
+        fi
         start_murano
+        if is_service_enabled murano-dsl; then
+            start_murano_dsl
+        fi
     fi
 
     if [[ "$1" == "unstack" ]]; then
         stop_murano
         if is_service_enabled horizon; then
             cleanup_murano_dashboard
+        fi
+        if is_service_enabled murano-dsl; then
+            stop_murano_dsl
+            cleanup_murano_dsl
         fi
     fi
 fi
